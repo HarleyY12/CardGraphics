182589232da05e760d7cfefab8a53300fdd4c146
HarleyY12 <hyang2496@gmail.com>
2025-04-21 17:16:45 -0400

    Update DrawPanel.java
    added array lists to keep track of highlighted cards and their indexes

--- src/DrawPanel.java
+++ src/DrawPanel.java
@@ -22,17 +22,19 @@ class DrawPanel extends JPanel implements MouseListener {
         this.addMouseListener(this);
         deck = Card.buildDeck();
         hand = buildHandFromDeck();
-        playAgain = new Rectangle(100,250,160,26);
+        playAgain = new Rectangle(100, 250, 160, 26);
         gameOver = false;
     }
+
     public ArrayList<Card> buildHandFromDeck() {
         ArrayList<Card> newHand = new ArrayList<Card>();
         for (int i = 0; i < 9 && !deck.isEmpty(); i++) {
-            int r = (int)(Math.random() * deck.size());
+            int r = (int) (Math.random() * deck.size());
             newHand.add(deck.remove(r));
         }
         return newHand;
     }
+
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         int x = 50;
@@ -51,7 +53,7 @@ class DrawPanel extends JPanel implements MouseListener {
             g.drawImage(c.getImage(), x, y, null);
             x = x + c.getImage().getWidth() + 10;
 
-            if((i+1) % 3 == 0){
+            if ((i + 1) % 3 == 0) {
                 x = 50;
                 y = y + c.getImage().getHeight() + 10;
             }
@@ -62,7 +64,7 @@ class DrawPanel extends JPanel implements MouseListener {
         // includes a border around the text
         g.setFont(new Font("Courier New", Font.BOLD, 20));
         g.drawString("GET NEW CARDS", 275, 50);
-        g.drawRect((int)button.getX(), (int)button.getY(), (int)button.getWidth(), (int)button.getHeight());
+        g.drawRect((int) button.getX(), (int) button.getY(), (int) button.getWidth(), (int) button.getHeight());
 
         g.drawString("Cards left: " + deck.size(), 50, 450);
         if (deck.isEmpty() && hand.isEmpty()) {
@@ -73,16 +75,17 @@ class DrawPanel extends JPanel implements MouseListener {
             g.drawString("You lose!", 130, 390);
             gameOver = true;
         }
-        g.drawString("Play again",120,270);
-        g.drawRect((int)playAgain.getX(), (int)playAgain.getY(),
-                    (int)playAgain.getWidth(), (int)playAgain.getHeight());
+        g.drawString("Play again", 120, 270);
+        g.drawRect((int) playAgain.getX(), (int) playAgain.getY(),
+                (int) playAgain.getWidth(), (int) playAgain.getHeight());
 
 
     }
+
     public boolean hasValidMove() {
-        for (int i = 0; i< hand.size();i++){
-            for (int j = i + 1; j < hand.size();j++){
-                if(getCardNumber(hand.get(i)) + getCardNumber(hand.get(j)) == 11){
+        for (int i = 0; i < hand.size(); i++) {
+            for (int j = i + 1; j < hand.size(); j++) {
+                if (getCardNumber(hand.get(i)) + getCardNumber(hand.get(j)) == 11) {
                     return true;
                 }
             }
@@ -91,25 +94,25 @@ class DrawPanel extends JPanel implements MouseListener {
         boolean hasQ = false;
         boolean hasK = false;
         for (Card c : hand) {
-            if (c.getValue().equals("J")){
+            if (c.getValue().equals("J")) {
                 hasJ = true;
             }
-            if (c.getValue().equals("Q")){
+            if (c.getValue().equals("Q")) {
                 hasQ = true;
             }
-            if (c.getValue().equals("K")){
+            if (c.getValue().equals("K")) {
                 hasK = true;
             }
         }
         return hasJ && hasQ && hasK;
     }
 
-    public int getCardNumber(Card cards){
+    public int getCardNumber(Card cards) {
         String value = cards.getValue();
-        if(value.equals("A")){
+        if (value.equals("A")) {
             return 1;
         }
-        if(value.equals("J") || value.equals("Q") || value.equals("K")){
+        if (value.equals("J") || value.equals("Q") || value.equals("K")) {
             return 0;
         }
         return Integer.parseInt(value);
@@ -126,12 +129,56 @@ class DrawPanel extends JPanel implements MouseListener {
             // evaluates if mouse clicked the coordinates
             // within the button
             if (button.contains(clicked)) {
-                hand = buildHandFromDeck();
-                gameOver = false;
+                ArrayList<Integer> selectedIndexes = new ArrayList<>();
+                ArrayList<Card> selectedCards = new ArrayList<>();
+
+                for (int i = 0; i < hand.size(); i++) {
+                    if (hand.get(i).getHighlight()) {
+                        selectedIndexes.add(i);
+                        selectedCards.add(hand.get(i));
+                    }
+                }
+
+                boolean valid = false;
+                if (selectedCards.size() == 2) {
+                    int sum = getCardNumber(selectedCards.get(0)) + getCardNumber(selectedCards.get(1));
+                    valid = (sum == 11);
+
+                } else if (selectedCards.size() == 3) {
+                    boolean hasJ = false;
+                    boolean hasK = false;
+                    boolean hasQ = false;
+                    for (Card c : selectedCards) {
+                        if (c.getValue().equals("J")) {
+                            hasJ = true;
+                        }
+                        if (c.getValue().equals("K")) {
+                            hasK = true;
+                        }
+                        if (c.getValue().equals("Q")) {
+                            hasQ = true;
+                        }
+                    }
+                    valid = hasJ && hasQ && hasK;
+                }
+
+                if (valid) {
+                    for (int i = 0; i < selectedIndexes.size(); i++) {
+                        int index = selectedIndexes.get(i);
+                        if (!deck.isEmpty()) {
+                            int random = (int) (Math.random() * deck.size());
+                            Card newCard = deck.remove(random);
+                            newCard.setHighlight(false);
+                            hand.set(index, newCard);
+                        }
+                    }
+                }
             }
-            if(playAgain.contains(clicked)){
+
+            if (playAgain.contains(clicked)) {
                 deck = Card.buildDeck();
                 hand = buildHandFromDeck();
+                gameOver = false;
             }
 
             // checks if any card was clicked
@@ -154,13 +201,19 @@ class DrawPanel extends JPanel implements MouseListener {
             }
         }
 
-
-
-
+        repaint();
     }
 
-    public void mouseReleased(MouseEvent e) { }
-    public void mouseEntered(MouseEvent e) { }
-    public void mouseExited(MouseEvent e) { }
-    public void mouseClicked(MouseEvent e) { }
+
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    public void mouseExited(MouseEvent e) {
+    }
+
+    public void mouseClicked(MouseEvent e) {
+    }
 }
